d12ec44afefb81eedc8a066167d274aacc8851d8
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-01-27 09:57:05 -0300

    Updates image

--- pages/_app.js
+++ pages/_app.js
@@ -41,8 +41,8 @@ export default function App({ Component, pageProps }) {
         <meta property="og:description" content="Next.js + StyledComponents" />
         <meta property="og:image" content={db.bg} />
         <meta property="og:image:type" content="image/jpg" />
-        <meta property="og:image:width" content="2550" />
-        <meta property="og:image:height" content="1700" />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
         <meta property="og:type" content="website" />
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
