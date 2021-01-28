8cb5379055279407a2448245ab2f71970259cfc5
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-01-27 22:55:27 -0300

    Changes color

--- src/components/widget.js
+++ src/components/widget.js
@@ -28,7 +28,7 @@ Widget.Header = styled.header`
   align-items: center;
   padding: 18px 32px;
   background-color: ${({ theme }) => theme.colors.primary};
-  color: white;
+  color: #ffffff;
 
   * {
     margin: 0;
